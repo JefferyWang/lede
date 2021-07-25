ddc8dd19e3e7f8f605640c404a3cfd9780889c4a
jinjin327 <32284406+jinjin327@users.noreply.github.com>
2021-07-25 11:40:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,7 +19,7 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.de
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 mkdir package/luci-app-openclash
 cd package/luci-app-openclash
 git init
@@ -37,3 +37,5 @@ rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 
 git clone https://github.com/lisaac/luci-app-dockerman.git
+git clone https://github.com/lisaac/luci-in-docker.git
+git clone https://github.com/destan19/OpenAppFilter.git
