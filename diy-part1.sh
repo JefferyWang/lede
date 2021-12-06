d60bb2e2e19fa794c1cc496803d9ff6b0c50cd61
jinjin327 <32284406+jinjin327@users.noreply.github.com>
2021-12-06 16:48:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,17 +17,12 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-
 cd package/lean
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 rm -rf luci-theme-argon
-rm -rf luci-app-dockerman
 git clone https://github.com/fw876/helloworld.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-git clone https://github.com/lisaac/luci-app-dockerman.git
-git clone https://github.com/lisaac/luci-in-docker.git
 git clone https://github.com/destan19/OpenAppFilter.git
 git clone https://github.com/jefferymvp/luci-app-koolproxyR
 git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
